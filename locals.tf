1f3b1a360cbf7e27413a34dcf459ea443581f1b2
Ezequiel Suasnabar <ezequiel_suasnabar@epam.com>
2026-05-25 20:19:05 -0300

    Task 1 files moved to a separated folder

--- locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  public_subnets = {
-    "a" = { cidr = "${var.public_subnet_cidr_1}", az_index = 0 }
-    "b" = { cidr = "${var.public_subnet_cidr_2}", az_index = 1 }
-    "c" = { cidr = "${var.public_subnet_cidr_3}", az_index = 2 }
-  }
-}
