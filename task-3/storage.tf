4f9ae3799f6bd9a60a7d95a1ed453da942f3d5a3
Ezequiel Suasnabar <ezequiel_suasnabar@epam.com>
2026-05-25 21:34:58 -0300

    Task 3: AWS IaC with Terraform: Create an Object Storage

---
+++ task-3/storage.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "cmtr-d3wf0oa8-bucket-1779754616"
+
+  tags = {
+    Project = "cmtr-d3wf0oa8"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
